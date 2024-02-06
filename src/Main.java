e1f644e86e79e42f96fc84ce7bd8b1aabf3693ff
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-02-06 15:04:31 +0100

    Menu

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n1 ;
+        int n2 ;
+        Scanner in = new Scanner(System.in);
+        String s=in.nextLine();
+        System.out.println("enter the name "+s);
+    }
+}
---
+++ src/Menu.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Menu {
+        
+    public static void main(String[] agrs){
+        System.out.println("1. Add un livre");
+        System.out.println("2. Remove Book");
+        System.out.println("3. Search Books");
+        System.out.println("4. Add Student");
+        System.out.println("5. Remove Student");
+        System.out.println("6. Display Books");
+        System.out.println("7. Display Students");
+        System.out.println("8. Quit");
+
+    }
+}
+
