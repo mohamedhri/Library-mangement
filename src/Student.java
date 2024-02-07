78b954ec2ef3066c6243165d81987fb94f3e7a18
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-02-07 16:35:47 +0100

    class student v1

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,17 @@
 import com.sun.javaws.jnl.LibraryDesc;
 
+import java.util.ArrayList;
+
+public class Student {
+    public String name;
+    public String address ;
+    public String idNumber ;
+    Public List <Book> borrowedBooks;
+
+    public Student (String name, String address, String idNumber){
+        this.name = name;
+        this.adress = adress;
+        this.idNumber = idNumber;
+        this.borrowedBooks =  new ArrayList<>();
+    }
+}
