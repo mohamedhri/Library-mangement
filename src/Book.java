bf4e11f34cb569699e5733f22db0a065cd4b4dde
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-02-07 14:52:14 +0100

    class book

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,25 @@
 public class Book {
+    public string title;
+    public string author;
+    public string isbn;
+    public string publicationDate;
+    public student currentBorrower;
+
+    public Book(String title, String author,String isbn, String publicationDate){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publicationDate = publicationDate;
+
+    }
+    public void modifyDetails(String title, String author, String isbn, String publicationDate){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publicationDate = publicationDate;
+    }
+
+    }
+
+
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
+import java.util.List;
+
 public class Menu {
-        
-    public static void main(String[] agrs){
-        System.out.println("1. Add un livre");
-        System.out.println("2. Remove Book");
-        System.out.println("3. Search Books");
-        System.out.println("4. Add Student");
-        System.out.println("5. Remove Student");
-        System.out.println("6. Display Books");
-        System.out.println("7. Display Students");
-        System.out.println("8. Quit");
-
+    public void displayOptions() {
+        System.out.println("__________________________");
+        System.out.println("***BOOK MANGEMENT MENU***");
+        System.out.println("--------------------------");
+        System.out.println("    1. Add a book");
+        System.out.println("    2. Remove a book");
+        System.out.println("    3. Search for books");
+        System.out.println("    4. Show list of books");
+        // Add more options as needed
+        System.out.println("    0. Exit");
     }
-}
 
+}
