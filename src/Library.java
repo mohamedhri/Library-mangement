df549237994cb2d8817c37fab656b31982d1f4d6
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-02-08 11:45:00 +0100

    class Library.v1

--- src/Library.java
+++ src/Library.java
@@ -5,4 +5,26 @@ public class Library {
     public List<Book> books;
     public List<Student> students;
 
+    public Library() {
+        this.books = new ArrayList<>();
+        this.students = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        books.remove(book);
+    }
+
+    public List<Book> searchBooks(String query) {
+        List<Book> result = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getTitle().toLowerCase().contains(query.toLowerCase())) {
+                result.add(book);
+            }
+        }
+        return result;
+    }
 }
